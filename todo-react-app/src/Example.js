d433205b6394136981fa0b29f0b26bae23cadc2a
mk0537 <xodus2331@naver.com>
2025-04-16 18:15:29 +0900

    Props & Hook example

--- todo-react-app/src/Example.js
+++ todo-react-app/src/Example.js
@@ -1,12 +1,23 @@
 import { useState } from "react"
 let Example= () => {
     const [message, setMessage] = useState("Hello, World");
+    const [name, setName] = useState("홍길동");
+    const [count, setCount] = useState(0);
     return (
         
         <div>
-            <p>{message}</p>
+            <p>{message} / {name}</p>
             {/*상태는 무조건 setter를 이용하여 변경해야 한다 */}
-            <button onClick={() => setMessage("Hello, React")}>Change message</button>
+            <button onClick={() =>{ 
+            if(message === "Hello, World"){
+                setMessage("Hello, React");
+            }else{
+                setMessage("Hello, World");
+            }
+            }}>Change message</button>
+
+            <p>Count: {count}</p>
+            <button onClick={()=>setCount(count+1)}>Increase</button>
         </div>
     )
 }
