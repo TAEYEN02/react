72c3c087803eba7febfcbee6a52aa1a9c9fb254a
mk0537 <xodus2331@naver.com>
2025-04-16 17:57:01 +0900

    First react

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
+import Example from './Example';
 
 function App() {
   return (
     <div className="App">
-      <Todo/>
+      <Example /> 
     </div>
   );
 }
--- todo-react-app/src/Example.js
+++ todo-react-app/src/Example.js
@@ -1,8 +1,13 @@
 import { useState } from "react"
 let Example= () => {
+    const [message, setMessage] = useState("Hello, World");
     return (
+        
         <div>
             <p>{message}</p>
+            {/*상태는 무조건 setter를 이용하여 변경해야 한다 */}
+            <button onClick={() => setMessage("Hello, React")}>Change message</button>
         </div>
     )
 }
+export default Example;
